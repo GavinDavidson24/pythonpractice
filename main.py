3fbd6ab621ba48acfd6a0b39a5393446130ce059
GavinDavidson24 <818399@mystma.org>
2021-09-16 15:19:24 -0500

    changes

---
+++ main.py
@@ -0,0 +1,45 @@
+
+def colors():
+    print("red")
+    print("blue")
+    print("orange")
+    print("green")
+
+colors()
+
+print(420 + 69)
+print(420 // 69)
+print(420 * 69)
+print(420 - 69)
+
+print("cat" * 500)
+
+def myNameIs(jabovi):
+    print( jabovi + " is my name")
+
+myNameIs("gavin")
+myNameIs("brug")
+myNameIs("jabovi")
+
+def myNameis(caymin):
+    print(caymin + "is my name :)")
+def timesTen(num):
+    print(num * 10)
+
+def plusFive(num):
+    print(num + 5)
+def dividetwo(num):
+    print(num / 2)
+
+def twoNumbas(x, y):
+    print(x + y)
+
+twoNumbas(420, 69)
+
+
+
+def madLibs(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
+    print("while i've stayed at home, i've practiced the" + a + "for" + b + "hours every" + c + ". My favorite key signature to play and practice in is" + d + e + ". It has" + f + "flats and" + g + "sharps. I like this key signature because it is" + h + "and" + i + ". I've also gotten awesome at counting time signatures. The time signature I love is" + j + ", because it is" + k + "to count. One instrument I want to learn how to play while I'm at home is" + l + ", because it is" + m + "and sounds like a" + n + ". My" + o + "likes it when I play" + p + ", and always gives me a round of applause after my perfornances! When I return to school, my teacher will be" + q + "of how great I am at playing my instrument. My teacher might even think I sould like" + r + "!")
+
+
+madLibs("flute", "25", "sunday", "c", "minor", "17", "1" , "cool", )
